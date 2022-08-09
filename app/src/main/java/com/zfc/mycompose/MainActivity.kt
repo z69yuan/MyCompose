cdeed60f29a44f6e6ebc0671db01d83e9218f2fa
69yuan <z69yuan@163.com>
2022-08-09 18:52:20 +0800

    reset

--- app/src/main/java/com/zfc/mycompose/MainActivity.kt
+++ app/src/main/java/com/zfc/mycompose/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainScreen()
+                    DemoScreen()
                 }
             }
         }
@@ -35,107 +35,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen() {
-    var linearSelected = remember {
-        mutableStateOf(true)
-    }
+fun DemoScreen() {
 
-
-    var imageSelected = remember {
-        mutableStateOf(true)
-    }
-
-    val onLinearClicked = { value : Boolean ->
-        linearSelected.value = value
-    }
-
-    val onTitleClicked = { value : Boolean ->
-        imageSelected.value = value
-    }
-
-    ScreenContent(
-        linearSelected = linearSelected.value,
-        imageSelected = imageSelected.value,
-        onLinearClick = onLinearClicked,
-        onTitleClick = onTitleClicked,
-        titleContent = {
-            if (imageSelected.value) {
-                TitleCompose(drawing = R.drawable.ic_baseline_cloud_download_24)
-            } else {
-                Text("Downloading", style = MaterialTheme.typography.bodyMedium,
-                    modifier = Modifier.padding(30.dp))
-            }
-        },
-        progressContent = {
-            if (linearSelected.value) {
-                LinearProgressIndicator(Modifier.height(40.dp))
-            } else {
-                CircularProgressIndicator(Modifier.size(200.dp),
-                    strokeWidth = 18.dp)
-            }
-        }
-    )
-}
-
-@Composable
-fun ScreenContent(
-    linearSelected: Boolean,
-    imageSelected: Boolean,
-    onLinearClick: (Boolean) -> Unit,
-    onTitleClick: (Boolean) -> Unit,
-    titleContent: @Composable () -> Unit,
-    progressContent: @Composable () -> Unit
-) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceBetween
-    ) {
-        titleContent()
-        progressContent()
-        CheckBoxes(
-            linearSelected = linearSelected,
-            imageSelected = imageSelected,
-            onLinearClick = onLinearClick,
-            onTitleClick = onTitleClick
-        )
-    }
-}
-
-
-@Composable
-fun CheckBoxes( linearSelected : Boolean ,
-               imageSelected : Boolean ,
-               onLinearClick : (Boolean) -> Unit ,
-               onTitleClick : (Boolean) -> Unit){
-
-    Row (Modifier.padding(20.dp),
-        verticalAlignment = Alignment.CenterVertically){
-        Checkbox(
-            checked = imageSelected,
-            onCheckedChange = onTitleClick
-        )
-        Text("Image Title")
-        Spacer(Modifier.width(20.dp))
-        Checkbox(checked = linearSelected,
-            onCheckedChange = onLinearClick
-        )
-        Text("Linear Progress")
-    }
-}
-
-@Composable
-fun TitleCompose(drawing: Int) {
-    Image(
-        painter = painterResource(id = drawing),
-        contentDescription = "title image"
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyComposeTheme {
-        MainScreen()
+        DemoScreen()
     }
 }
