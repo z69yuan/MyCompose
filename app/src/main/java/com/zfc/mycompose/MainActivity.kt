0cdf97f5fd6ec6848d5e98a1d701728d994d7daf
69yuan <z69yuan@163.com>
2022-08-09 17:30:35 +0800

    修改Compose版本

--- app/src/main/java/com/zfc/mycompose/MainActivity.kt
+++ app/src/main/java/com/zfc/mycompose/MainActivity.kt
@@ -3,13 +3,18 @@ package com.zfc.mycompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.zfc.mycompose.ui.theme.MyComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
@@ -30,14 +35,65 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    var linearSelected = remember {
+        mutableStateOf(true)
+    }
+
+    var imageSelected = remember {
+        mutableStateOf(true)
+    }
+
+    val onLinearClicked = { value : Boolean ->
+        linearSelected.value = value
+    }
+
+    val onTitleClicked = { value : Boolean ->
+        imageSelected.value = value
+    }
+
+
+}
+
+@Composable
+fun ScreenContent(
+    linearSelected : Boolean ,
+    imageSelected : Boolean ,
+    onLinearClick : (Boolean) -> Unit ,
+    onTitleClick : (Boolean) -> Unit
+){
+    Column(modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween) {
+
+    }
+}
+
+
+@Composable
+fun CheckBoxes( linearSelected : Boolean ,
+               imageSelected : Boolean ,
+               onLinearClick : (Boolean) -> Unit ,
+               onTitleClick : (Boolean) -> Unit){
+
+    Row (Modifier.padding(20.dp)){
+        Checkbox(
+            checked = imageSelected,
+            onCheckedChange = onTitleClick
+        )
+        Text("Image Title")
+        Spacer(Modifier.width(20.dp))
+//        Checkbox(checked = linearSelected,
+//            onCheckedChange = onLinearClick
+//        )
+        Text("Linear Progress")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyComposeTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
