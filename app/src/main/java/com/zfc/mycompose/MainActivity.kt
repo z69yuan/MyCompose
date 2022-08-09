8248acc8dabb901a52e40b1a1b7f406768ae9549
69yuan <z69yuan@163.com>
2022-08-09 18:50:26 +0800

    ok

--- app/src/main/java/com/zfc/mycompose/MainActivity.kt
+++ app/src/main/java/com/zfc/mycompose/MainActivity.kt
@@ -3,16 +3,16 @@ package com.zfc.mycompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.zfc.mycompose.ui.theme.MyComposeTheme
@@ -40,6 +40,7 @@ fun MainScreen() {
         mutableStateOf(true)
     }
 
+
     var imageSelected = remember {
         mutableStateOf(true)
     }
@@ -52,20 +53,52 @@ fun MainScreen() {
         imageSelected.value = value
     }
 
-
+    ScreenContent(
+        linearSelected = linearSelected.value,
+        imageSelected = imageSelected.value,
+        onLinearClick = onLinearClicked,
+        onTitleClick = onTitleClicked,
+        titleContent = {
+            if (imageSelected.value) {
+                TitleCompose(drawing = R.drawable.ic_baseline_cloud_download_24)
+            } else {
+                Text("Downloading", style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(30.dp))
+            }
+        },
+        progressContent = {
+            if (linearSelected.value) {
+                LinearProgressIndicator(Modifier.height(40.dp))
+            } else {
+                CircularProgressIndicator(Modifier.size(200.dp),
+                    strokeWidth = 18.dp)
+            }
+        }
+    )
 }
 
 @Composable
 fun ScreenContent(
-    linearSelected : Boolean ,
-    imageSelected : Boolean ,
-    onLinearClick : (Boolean) -> Unit ,
-    onTitleClick : (Boolean) -> Unit
-){
-    Column(modifier = Modifier.fillMaxSize(),
+    linearSelected: Boolean,
+    imageSelected: Boolean,
+    onLinearClick: (Boolean) -> Unit,
+    onTitleClick: (Boolean) -> Unit,
+    titleContent: @Composable () -> Unit,
+    progressContent: @Composable () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceBetween) {
-
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        titleContent()
+        progressContent()
+        CheckBoxes(
+            linearSelected = linearSelected,
+            imageSelected = imageSelected,
+            onLinearClick = onLinearClick,
+            onTitleClick = onTitleClick
+        )
     }
 }
 
@@ -76,18 +109,27 @@ fun CheckBoxes( linearSelected : Boolean ,
                onLinearClick : (Boolean) -> Unit ,
                onTitleClick : (Boolean) -> Unit){
 
-    Row (Modifier.padding(20.dp)){
+    Row (Modifier.padding(20.dp),
+        verticalAlignment = Alignment.CenterVertically){
         Checkbox(
             checked = imageSelected,
             onCheckedChange = onTitleClick
         )
         Text("Image Title")
         Spacer(Modifier.width(20.dp))
-//        Checkbox(checked = linearSelected,
-//            onCheckedChange = onLinearClick
-//        )
+        Checkbox(checked = linearSelected,
+            onCheckedChange = onLinearClick
+        )
         Text("Linear Progress")
     }
+}
+
+@Composable
+fun TitleCompose(drawing: Int) {
+    Image(
+        painter = painterResource(id = drawing),
+        contentDescription = "title image"
+    )
 }
 
 @Preview(showBackground = true)
