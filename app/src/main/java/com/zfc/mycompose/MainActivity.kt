58db77ee11e3cea0792331253cd1437a16297e14
69yuan <z69yuan@163.com>
2022-08-10 10:20:13 +0800

    自定义Modifier

--- app/src/main/java/com/zfc/mycompose/MainActivity.kt
+++ app/src/main/java/com/zfc/mycompose/MainActivity.kt
@@ -4,17 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.zfc.mycompose.ui.theme.MyComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,7 +39,34 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DemoScreen() {
+    val modifier = Modifier
+        .border(width = 2.dp, color = Color.Black)
+        .padding(20.dp)
 
+    Column(Modifier.padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+        Text(
+            "Hello Compose" ,
+            modifier,
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold,
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        CustomImage(drawing = R.drawable.dirk,
+            Modifier.padding(20.dp)
+                .clip(shape = RoundedCornerShape(30.dp)) //裁剪
+        )
+    }
+}
+
+@Composable
+fun CustomImage(drawing: Int , modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = drawing),
+        contentDescription = "图片",
+        modifier
+    )
 }
 
 @Preview(showBackground = true)
