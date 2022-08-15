3e09362ab1ab12ac84b50b911aec206e17d33eec
69yuan <z69yuan@163.com>
2022-08-15 15:14:26 +0800

    update

--- app/src/main/java/com/zfc/mycompose/MainActivity.kt
+++ app/src/main/java/com/zfc/mycompose/MainActivity.kt
@@ -21,6 +21,8 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.FirstBaseline
 import androidx.compose.ui.layout.LastBaseline
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
@@ -51,51 +53,52 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(){
-//    Box{
-//        val height = 200.dp
-//        val width = 200.dp
-//        TextCell("1",Modifier.size(width = width , height = height))
-//        TextCell("2",Modifier.size(width = width , height = height))
-//        TextCell("3",Modifier.size(width = width , height = height))
-//    }
+    Box(
+        modifier = Modifier.size(120.dp,80.dp)
+    ){
+        ClearBox(
+            Modifier
+                .offset()
+                .exampleLayout(20,40)
+                .background(Color.Blue)
 
-//    Box(modifier = Modifier.size(height = 90.dp, width = 290.dp)) {
-//        Text("TopStart", Modifier.align(Alignment.TopStart))
-//        Text("TopCenter", Modifier.align(Alignment.TopCenter))
-//        Text("TopEnd", Modifier.align(Alignment.TopEnd))
-//
-//        Text("CenterStart", Modifier.align(Alignment.CenterStart))
-//        Text("Center", Modifier.align(Alignment.Center))
-//        Text(text = "CenterEnd", Modifier.align(Alignment.CenterEnd))
-//
-//        Text("BottomStart", Modifier.align(Alignment.BottomStart))
-//        Text("BottomCenter", Modifier.align(Alignment.BottomCenter))
-//        Text("BottomEnd", Modifier.align(Alignment.BottomEnd))
-//    }
-
-    Bagua()
-
+        )
+    }
 }
+
+fun Modifier.exampleLayout(
+    x:Int,
+    y:Int
+) = layout { measurable, constraints ->
+    val placeable = measurable.measure(constraints)
+    layout(placeable.width,placeable.height){
+        placeable.placeRelative(x,y)
+    }
+}
+
+@Composable
+fun ClearBox(modifier : Modifier = Modifier) {
+    Box(
+        modifier = modifier.then(
+            Modifier
+                .padding(1.dp)
+                .width(50.dp)
+                .height(10.dp)
+        )
+    )
+}
+
 
 @Composable
 fun Bagua() {
-    Box(Modifier.size(210.dp)
-        .clip(CutCornerShape((210/3.4f).toInt().dp))
-        .background(Color.Blue))
+    Box(
+        Modifier
+            .size(210.dp)
+            .clip(CutCornerShape((210 / 3.4f).toInt().dp))
+            .background(Color.Blue))
 }
 
 
-@Composable
-fun TextCell(text:String, modifier:Modifier = Modifier , fontSize:Int = 150){
-
-    val cellModifier = Modifier.padding(5.dp)
-        .border(width = 5.dp, color = Color.Black)
-    Text(text = text,
-        modifier = cellModifier.then(modifier),
-        fontSize = fontSize.sp,
-        fontWeight = FontWeight.Bold,
-        textAlign = TextAlign.Center)
-}
 
 
 
